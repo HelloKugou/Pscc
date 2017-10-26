1eb7627027f772f413d1c0b889c1d08e24ba1e0f
root <root@localhost.localdomain>
2017-10-26 23:59:54 +0800

    邮件工具开发

--- utils/mail.py
+++ utils/mail.py
@@ -1,26 +1,94 @@
 #!/usr/bin/env python
 #-*-coding:utf-8*-
-#这个工具是邮件提醒工具
+#这个工具是每日邮件提醒工具
 
 
 #!/usr/bin/env python3
 #coding: utf-8
+# coding: utf-8
 import smtplib
+import mimetypes
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 from email.header import Header
+import email
 
-sender = 'linhanqiu1123@163.com'
-receiver = '386890866@qq.com'
-subject = 'python email test'
-smtpserver = 'smtp.163.com'
-username = 'linhanqiu1123@163.com'
-password = 'linhanqiu1123'
+#配置类
+class Cfg:
+    mail_config = {
+        "sender":"workinform@163.com",
+        "receiver":"linhanqiu1123@163.com",
+        "username":"workinform@163.com",
+        "password":"linhanqiu1123",
+        "smtpserver":"smtp.163.com"
+    }
+    sys_config = {
+        "path": "/home/linhanqiu/img/1.jpg"
+    }
 
-msg = MIMEText('你好','text','utf-8')#中文需参数‘utf-8'，单字节字符不需要
-msg['Subject'] = Header(subject, 'utf-8')
+#内容类
+class Content:
+    def __init__(self,id=None,word="今日数据正常，暂无反常数据产生",subject=u"每日通知"):
+        self.id = id
+        self.word = word
+        self.subject = subject
+        self.html = "<p1></p1>"
+    #内容格式，其他格式还在添加
+    def text(self):
+        msgRoot = MIMEMultipart("related")
+        msgRoot['Subject'] = Header(self.subject, 'utf-8')
+        msgRoot['From'] = Header('爬虫任务情况<workinform@163.com>','utf-8')
+        msgRoot['To'] = "linhanqiu1123@163.com"
+        msgRoot.preamble = 'This is a multi-part message in MIME format.'
+        #设置转化部分
+        msgAlternative = MIMEMultipart("alternative")
+        msgRoot.attach(msgAlternative)
+        #添加纯文本信息，（添加模板）
+        msgText = MIMEText(self.word,'plain','utf-8')
+        msgAlternative.attach(msgText)
+        #设定html信息，（添加模板）
+        msgHtml = MIMEText(self.html,'html','utf-8')
+        msgRoot.attach(msgHtml)
+        #设置图片信息,（matplotlib画图）
+        fp = open(Cfg.sys_config["path"], 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+        msgImage.add_header('Content-ID', '<image1>')
+        msgRoot.attach(msgImage)
+        return msgRoot
 
-smtp = smtplib.SMTP()
-smtp.connect('smtp.163.com')
-smtp.login(username, password)
-smtp.sendmail(sender, receiver, msg.as_string())
-smtp.quit()
+#邮件基类
+class Mail:
+    def __init__(self):
+        self.cfg = Cfg.mail_config
+        self.server = self.cfg["smtpserver"]
+        self.sender = self.cfg["sender"]
+        self.receiver = self.cfg["receiver"]
+        self.username = self.cfg["username"]
+        self.password = self.cfg["password"]
+    def login(self):
+        s = smtplib.SMTP()
+        s.connect(self.server)
+        s.login(self.username,self.password)
+        #调式过程
+        # s.set_debuglevel(1)
+        s.ehlo("begin connect")
+        # s.starttls()
+        return s
+    def send(self):
+        s = self.login()
+        #创建内容
+        m = Content().text()
+        s.sendmail(self.sender,self.receiver,m.as_string())
+        print("已发送")
+        s.getreply()
+        s.quit()
+    def __call__(self, *args, **kwargs):
+        return self.send()
+
+#商道邮件子类
+#娱道邮件子类
+if __name__=="__main__":
+    mail = Mail()
+    mail()
